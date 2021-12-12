434b164c9c8f9c97c854276671b4a741279192a1
nozimjonpro <nozimjonkeldiyev@yandex.ru>
2021-12-12 14:19:34 +0500

    Initial project with header section

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const elHamburger = document.querySelector('.hamburger');
+const elHeader = document.querySelector('.header');
+
+
+
+
+elHamburger.addEventListener('click', ()=>{
+    elHeader.classList.toggle('header--active')
+});
